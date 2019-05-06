9ecdf32ff1e4198b3d732009900541d3b0a13450
curtainTan <1948750473@qq.com>
2019-05-06 08:49:14 +0800

    添加搜索接口和modal

--- lib/config/server/api.dart
+++ lib/config/server/api.dart
@@ -19,10 +19,15 @@ const servicePath = {
   "userdj" : serviceUrl + "/user/dj",                      // 传入用户 id, 可以获取用户电台
   "songurl" : serviceUrl + "/song/url",                    // 传入的音乐 id 可选参数 : br: 码率 如果要 320k 则可设置为 320000,其他类推
   "checkmusic" : serviceUrl + "/check/music",              // 传入歌曲 id, 可获取音乐是否可用
+  
+  // 搜索
   "search" : serviceUrl + "/search",                       // 必选参数 : keywords : 关键词
                                                            // 可选参数 : limit : 返回数量 , 默认为 30 offset : 偏移数量，用于分页 , 如 : 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
                                                            // type: 搜索类型；默认为 1 即单曲 , 取值意义 : 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频
   "searchhot" : serviceUrl + "/search/hot",                // 获取热门搜索列表
+  "searchsuggest" : serviceUrl + "/search/suggest",        // 搜索建议  必选参数 : keywords : 关键词  可选参数 : type : 如果传 'mobile' 则返回移动端数据
+  "searchmultimatch" : serviceUrl + "/search/multimatch",  // 搜索多重匹配 必选参数 : keywords : 关键词  调用此接口 , 传入搜索关键词可获得搜索结果
+  
   "lyric" : serviceUrl + "/lyric",                         // 传入音乐 id 可获得对应音乐的歌词
 
 
