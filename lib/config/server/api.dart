8298553fa6e036102360f178e47ce6117b52f501
curtainTan <1948750473@qq.com>
2019-05-22 09:26:39 +0800

    添加mv部分的api和model

--- lib/config/server/api.dart
+++ lib/config/server/api.dart
@@ -32,6 +32,16 @@ const servicePath = {
   
   "lyric" : serviceUrl + "/lyric",                         // 传入音乐 id 可获得对应音乐的歌词
 
+  // Mv相关
+  "mvDetail" : serviceUrl + "/mv/detail",                  // 获取 mv 数据   必选参数 : mvid: mv 的 id
+  "mvUrl" : serviceUrl + "/mv/url",                        // 说明 : 调用此接口 , 传入 mv id,可获取 mv 播放地址     /mv/url?id=5436712
+  "relatedAllvideo" : serviceUrl + "/related/allvideo",    // 必选参数 : id: 视频 的 id     调用此接口 , 可获取相关视频
+  "videoDetail" : serviceUrl + "/video/detail",            // 视频详情    id: 视频 的 id
+  "videoUrl" : serviceUrl + "/video/url",                  // 调用此接口 , 传入视频 id,可获取视频播放地址      必选参数 : id: 视频 的 id
+  "commentMv" : serviceUrl + "/comment/mv",                // 调用此接口 , 传入音乐 id 和 limit 参数 , 可获得该 mv 的所有评论 ( 不需要 登录 )
+                                                           // limit: 取出评论数量 , 默认为 20   offset: 偏移数量 , 用于分页 , 如 :( 评论页数 -1)*20, 其中 20 为 limit 的值
+  "simiMv" : serviceUrl + "/simi/mv",                      // 相似 mv    ~说明 : 调用此接口 , 传入 mvid 可获取相似 mv   ~必选参数 : mvid: mv id   ~/simi/mv?mvid=5436712
+ 
 
 
 };
---
+++ lib/views/mv/mv_main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+
+
+
+
+
+
+
+class MvPage extends StatefulWidget {
+  @override
+  _MvPageState createState() => _MvPageState();
+}
+
+class _MvPageState extends State<MvPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
